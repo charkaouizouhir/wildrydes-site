10aabbd60c5395e8e1ef12cd12bc2b1bcda0b842
zouhir charkaoui <149547536+charkaouizouhir@users.noreply.github.com>
2025-04-19 00:06:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_0dGKDgfWE', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '6ba9e761kk2okkeebelqjbmnlu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        clientSecret: 's56t3728dqbulefookg7lbi54b96on27l1r3263l3mp24rcvjs4'
         region: 'us-east-1' // e.g. us-east-2
+        
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
